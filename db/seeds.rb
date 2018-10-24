5d1e5912c9abb997e5c20bab206d6a092585c7e9
Cristina Simoncini <crisimonc@gmail.com>
2018-10-24 16:29:53 +0200

    Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "rest1", phone_number: "333 444", address: "XX Street", category: "chinese")
+Restaurant.create(name: "rest2", phone_number: "333 444", address: "XX Street", category: "italian")
+Restaurant.create(name: "rest3", phone_number: "333 444", address: "XX Street", category: "japanese")
+Restaurant.create(name: "rest4", phone_number: "333 444", address: "XX Street", category: "french")
+Restaurant.create(name: "rest5", phone_number: "333 444", address: "XX Street", category: "belgian")
+
+10.times do
+  Review.create(content: "blabla", rating: 3, restaurant_id: rand(1..5))
+end
